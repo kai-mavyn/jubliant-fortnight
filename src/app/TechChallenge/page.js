eca8345db2143d556a9c6ebec436505cf572b9a9
Kai Mavyn <kaimavyn@Kais-MacBook-Pro.local>
2023-11-09 13:56:24 -0800

    cleaning up files

--- src/app/TechChallenge/page.js
+++ src/app/TechChallenge/page.js
@@ -1,4 +1,5 @@
 // `app/techchallenge/page.js` is the UI for the `/techchallenge` URL
 export default function Page() {
-    return <h1>Hello, TechChallenge Page!</h1>
+    return <h1>Hello! Nothing to see here but us chickens!</h1>
+        
 }
